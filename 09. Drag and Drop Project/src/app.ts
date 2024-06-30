401c2a7a709b09c3fae60da173cd31f399c68c24
RRFayad <renan.r.fayad@gmail.com>
2024-06-29 21:14:44 -0300

    S09 (Drag & Drop Project): 124. Interacting with DOM Elements

--- 09. Drag and Drop Project/src/app.ts
+++ 09. Drag and Drop Project/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.querySelector("#project-input")! as HTMLTemplateElement;
@@ -9,7 +12,23 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector("#title")! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector("#description")! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector("#people")! as HTMLInputElement;
+
     this.attach();
+    this.configure();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
